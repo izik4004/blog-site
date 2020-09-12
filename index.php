f252cb2132de0d766c5dda807c0bf4797e9b68d7
Izik <49079739+izik4004@users.noreply.github.com>
2020-09-12 23:47:13 +0100

    added search bar

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!-- font awesome --->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+   <!---google font --->
+    <link href="https://fonts.googleapis.com/css2?family=Candal&family=Lora:wght@500&display=swap" rel="stylesheet">
+    <!--custom styling---->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+<div class="logo">
+    <h1 class="logo-text"><span>Places</span></h1>
+</div>
+<i class="far fa far fa-bars menu-toggle"></i>
+<ul class="nav">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+   <!---- <li><a href="#">Sign Up</a></li> ---->
+   <!---- <li><a href="#">Login</a></li> ----->
+    <li>
+        <a href="#">
+        <i class="far fa far fa-user"></i>
+        Izik
+        <i class="far fa far fa-chevron-down drop-down" ></i>
+    </a>  
+    <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#" class="logout">Logout</a></li>
+    </ul>
+    </li>
+</ul>
+    </header>
+
+<div class="page-wrapper">
+
+<div class="post-slider">
+    <h1 class="slider-title">Trending Post</h1>
+    <i class="fas far fa-chevron-left prev"></i>
+    <i class="fas far fa-chevron-right next"></i>
+    <div class="post-wrapper">
+    <div class="post">
+        <img src="assets/img/nike4.jpg" alt="nike lake" class="slider-image">
+        <div class="post-info">
+            <h4 class="single html">i luv you</h4>
+            <i class="far far fa-user">Izik </i>
+            &nbsp;
+            <i class="far f-calender"> Mar 8, 2019 </i>
+        </div>
+    </div>
+   
+    <div class="post">
+        <img src="assets/img/nike4.jpg" alt="" class="slider-image">
+        <div class="post-info">
+            <h4 class="single html">i luv you</h4>
+            <i class="far far fa-user">Izik </i>
+            &nbsp;
+            <i class="far f-calender"> Mar 8, 2019 </i>
+        </div>
+    </div>
+   
+    <div class="post">
+        <img src="assets/img/nike4.jpg" alt="" class="slider-image">
+        <div class="post-info">
+            <h4 class="single html">i luv you</h4>
+            <i class="far far fa-user">Izik </i>
+            &nbsp;
+            <i class="far f-calender"> Mar 8, 2019 </i>
+        </div>
+    </div>
+   
+    <div class="post">
+        <img src="assets/img/nike4.jpg" alt="" class="slider-image">
+        <div class="post-info">
+            <h4 class="single html">i luv you</h4>
+            <i class="far far fa-user">Izik </i>
+            &nbsp;
+            <i class="far f-calender"> Mar 8, 2019 </i>
+        </div>
+    </div>
+
+    <div class="post">
+        <img src="assets/img/nike4.jpg" alt="" class="slider-image">
+        <div class="post-info">
+            <h4 class="single html">i luv you</h4>
+            <i class="far far fa-user">Izik </i>
+            &nbsp;
+            <i class="far f-calender"> Mar 8, 2019 </i>
+        </div>
+    </div>
+    </div>
+</div>
+
+<div class="content clearfix">
+
+<div class="main-content">
+    <h2 class="recent-post-title">Recent Post</h2>
+
+<div class="post">
+<img src="assets/img/nike4.jpg" alt="" class="post-image">
+<div class="post-preview">
+    <h1><a href="single.html">. jhkjhkhlkjhlkhlkjlkjlkj;ljdsjh</a></h1>
+    <i class="far far fa-user"> Izik </i>
+   &nbsp;
+    <i class="far fa-calender"> Sep 11, 2020 </i>
+    <p class="preview-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam doloribus perspiciatis nihil, delectus, possimus sed aperiam vero quod deserunt sunt magni facilis animi voluptate nisi. Fugiat vitae itaque architecto sequi!
+    </p>
+    <a href="single.html" class="btn read-more"> Read More</a>
+</div>
+</div>
+
+<div class="post">
+<img src="assets/img/nike4.jpg" alt="" class="post-image">
+<div class="post-preview">
+    <h1><a href="single.html">. jhkjhkhlkjhlkhlkjlkjlkj;ljdsjh</a></h1>
+    <i class="far far fa-user"> Izik </i>
+   &nbsp;
+    <i class="far fa-calender"> Sep 11, 2020 </i>
+    <p class="preview-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam doloribus perspiciatis nihil, delectus, possimus sed aperiam vero quod deserunt sunt magni facilis animi voluptate nisi. Fugiat vitae itaque architecto sequi!
+    </p>
+    <a href="single.html" class="btn read-more"> Read More</a>
+</div>
+</div>
+
+<div class="post">
+<img src="assets/img/nike4.jpg" alt="" class="post-image">
+<div class="post-preview">
+    <h1><a href="single.html">. jhkjhkhlkjhlkhlkjlkjlkj;ljdsjh</a></h1>
+    <i class="far far fa-user"> Izik </i>
+   &nbsp;
+    <i class="far fa-calender"> Sep 11, 2020 </i>
+    <p class="preview-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam doloribus perspiciatis nihil, delectus, possimus sed aperiam vero quod deserunt sunt magni facilis animi voluptate nisi. Fugiat vitae itaque architecto sequi!
+    </p>
+    <a href="single.html" class="btn read-more"> Read More</a>
+</div>
+</div>
+
+<div class="post">
+<img src="assets/img/nike4.jpg" alt="" class="post-image">
+<div class="post-preview">
+    <h1><a href="single.html">. jhkjhkhlkjhlkhlkjlkjlkj;ljdsjh</a></h1>
+    <i class="far far fa-user"> Izik </i>
+   &nbsp;
+    <i class="far fa-calender"> Sep 11, 2020 </i>
+    <p class="preview-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam doloribus perspiciatis nihil, delectus, possimus sed aperiam vero quod deserunt sunt magni facilis animi voluptate nisi. Fugiat vitae itaque architecto sequi!
+    </p>
+    <a href="single.html" class="btn read-more"> Read More</a>
+</div>
+</div>
+
+<div class="post">
+<img src="assets/img/nike4.jpg" alt="" class="post-image">
+<div class="post-preview">
+    <h1><a href="single.html">. jhkjhkhlkjhlkhlkjlkjlkj;ljdsjh</a></h1>
+    <i class="far fa-user"> Izik </i>
+   &nbsp;
+    <i class="far fa-calender"> Sep 11, 2020 </i>
+    <p class="preview-text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam doloribus perspiciatis nihil, delectus, possimus sed aperiam vero quod deserunt sunt magni facilis animi voluptate nisi. Fugiat vitae itaque architecto sequi!
+    </p>
+    <a href="single.html" class="btn read-more"> Read More</a>
+</div>
+</div>
+
+</div>
+<div class="sidebar">
+    <div class="section search">
+        <h2 class="section-title">Search</h2>
+        <form action="index.html" method="post">
+            <input type="text" name="search-term" class="text-input" placeholder="search">
+        </form>
+</div>
+
+<div class="section topics">
+
+<h2 class="section-title">Topics</h2>
+<ul>
+<li><a href="#">Nature</a></li>    
+<li><a href="#">Nature</a></li>  
+<li><a href="#">Nature</a></li>  
+<li><a href="#">Nature</a></li>  
+<li><a href="#">Nature</a></li>  
+<li><a href="#">Nature</a></li>  
+<ul>
+</div>
+    </div>
+</div>
+</div>
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+<script src="assets/js/scripts.js"> </script>
+</body>
+</html>
